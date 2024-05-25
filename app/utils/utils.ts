2fdc09ee12be140066be6b3d0f97d8bd010d5264
Michael Ross Tarr <mrmrmtarr@gmail.com>
2024-05-25 22:05:29 +0200

    Added basic util functions
    Converting decimal and hexadecimal

---
+++ app/utils/utils.ts
@@ -0,0 +1,19 @@
+/**
+ * Util function to convert any string to a decimal, its easier to do calculations on a decimal than a hexadecimal.
+ * @param value Is the value that you want to convert to decimal.
+ * @returns A decimal.
+ */
+const convertToDecimal = (value: string): number => {
+    return parseInt(value);
+}
+
+/**
+ * Util function to convert any decimal number to hexadecimal to display to the user.
+ * @param value The decimal value you want to convert to hexadecimal.
+ * @returns A hexadecimal.
+ */
+const convertToHexadecimal = (value: number): string => {
+    return value.toString();
+}
+
+export { convertToDecimal, convertToHexadecimal };
